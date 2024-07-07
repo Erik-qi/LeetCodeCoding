1ef2822cc238325793e6ecee45694489aa8a1846
qihaoyu <qihaoyu5988@outlook.com>
2024-07-07 21:10:08 +0800

    [提交]《程序员面试金典》 01.01. 判定字符是否唯一

---
+++ 01.cpp
@@ -0,0 +1,34 @@
+/**********************************************
+* 文件名：01.cpp
+* 创建人：qhy
+* 创建时间：2024.07.02
+* 题目来源：程序员面试金典
+* 代码描述：01.01 判定字符是否唯一(https://leetcode.cn/studyplan/cracking-the-coding-interview/)
+***********************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isUnique(string astr) {
+        if (astr == "")
+            return true;
+        sort(astr.begin(), astr.end());
+        for (int i = 1; i < astr.length(); i++) {
+            if (astr[i] == astr[i - 1])
+                return false;
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string s = "leetcode";
+    cout << sol.isUnique(s);
+}
